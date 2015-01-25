0a9df9d98a8f3a0cb92cffae8d7fff63e10bd2d9
Daniel Puleri <daniel.puleri@gatech.edu>
2015-01-24 19:16:07 -0500

    Initial push.

---
+++ WinRedditWallpaper/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+
+using System.Diagnostics;
+
+namespace WinRedditWallpaper
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string subredditRawText;
+        public MainWindow()
+        {
+            InitializeComponent();
+            //PicScraper.scrape();
+        }
+
+        private void SubredditsTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            subredditRawText = SubredditsTextBox.Text;
+        }
+
+        private void GetSubredditPics_Click(object sender, RoutedEventArgs e)
+        {
+            if (subredditRawText != null)
+            {
+                subredditRawText = subredditRawText.Trim().Replace(" ", "");
+                PicScraper.scrape(subredditRawText.Split(new string[] { ";" }, StringSplitOptions.RemoveEmptyEntries));
+            }
+        }
+
+        private void ChooseDir_Click(object sender, RoutedEventArgs e)
+        {
+            //Microsoft.Win32.OpenFileDialog browse = new Microsoft.Win32.OpenFileDialog();
+            FolderBrowserDialog browse = new FolderBrowserDialog();
+            System.Windows.Forms.DialogResult result = browse.ShowDialog();
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                string path = browse.SelectedPath;
+                Debug.WriteLine(path);
+            }
+        }
+    }
+}
---
+++ WinRedditWallpaper/PicScraper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Diagnostics;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace WinRedditWallpaper
+{
+    public static class PicScraper
+    {
+        public static void scrape(string[] subreddits)
+        {
+            // Scrape links from wikipedia.org
+
+            // 1.
+            //
+            Debug.WriteLine(subreddits.Length);
+            for (int i = 0; i < subreddits.Length; i++)
+            {
+                WebClient w = new WebClient();
+                string page = null;
+                try
+                {
+                    page = w.DownloadString("http://reddit.com/r/" + subreddits[i]);
+                    //Debug.WriteLine(page);
+                    //positive lookbehind (?<=text)
+                    //positive lookahead q(?=u)
+                }
+                catch (System.Net.WebException e)
+                {
+                    Debug.WriteLine(e.Message);
+                }
+
+                if (page != null)
+                {
+                    string pat = @"(?<=<a class=""title may-blank "" href="")(.{0,200}\.[a-z]{3}?)(?="" tabindex=""1"" >)";
+
+                    //this will help http://msdn.microsoft.com/en-us/library/ez801hhe%28v=vs.110%29.aspx
+
+                    MatchCollection matches = Regex.Matches(page, pat);
+                    foreach (Match match in matches)
+                    {
+                        Debug.WriteLine(match.Value);
+                    }
+                    Debug.WriteLine(matches.Count);
+                }
+            }
+        }
+    }
+}
